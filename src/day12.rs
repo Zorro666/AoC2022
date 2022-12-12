63d9803f788ee7b743339e4ad58ca6e766c6f242
Jake Turner <jake@evansturner.co.uk>
2022-12-12 07:53:10 +0000

    Day12 done

--- src/day12.rs
+++ src/day12.rs
@@ -2,6 +2,70 @@ use crate::file_to_vec;
 
 /*
 
+--- Day 12: Hill Climbing Algorithm ---
+
+You try contacting the Elves using your handheld device, but the river you're following must be too low to get a decent signal.
+
+You ask the device for a heightmap of the surrounding area (your puzzle input).
+The heightmap shows the local area from above broken into a grid; the elevation of each square of the grid is given by a single lowercase letter, where a is the lowest elevation, b is the next-lowest, and so on up to the highest elevation, z.
+
+Also included on the heightmap are marks for your current position (S) and the location that should get the best signal (E).
+Your current position (S) has elevation a, and the location that should get the best signal (E) has elevation z.
+
+You'd like to reach E, but to save energy, you should do it in as few steps as possible.
+During each step, you can move exactly one square up, down, left, or right.
+To avoid needing to get out your climbing gear, the elevation of the destination square can be at most one higher than the elevation of your current square; that is, if your current elevation is m, you could step to elevation n, but not to elevation o.
+(This also means that the elevation of the destination square can be much lower than the elevation of your current square.)
+
+For example:
+
+Sabqponm
+abcryxxl
+accszExk
+acctuvwj
+abdefghi
+Here, you start in the top-left corner; your goal is near the middle.
+You could start by moving down or right, but eventually you'll need to head toward the e at the bottom.
+From there, you can spiral around to the goal:
+
+v..v<<<<
+>v.vv<<^
+.>vv>E^^
+..v>>>^^
+..>>>>>^
+In the above diagram, the symbols indicate whether the path exits each square moving up (^), down (v), left (<), or right (>).
+The location that should get the best signal is still E, and .  marks unvisited squares.
+
+This path reaches the goal in 31 steps, the fewest possible.
+
+What is the fewest steps required to move from your current position to the location that should get the best signal?
+
+Your puzzle answer was 350.
+
+--- Part Two ---
+
+As you walk up the hill, you suspect that the Elves will want to turn this into a hiking trail. The beginning isn't very scenic, though; perhaps you can find a better starting point.
+
+To maximize exercise while hiking, the trail should start as low as possible: elevation a. The goal is still the square marked E. However, the trail should still be direct, taking the fewest steps to reach its goal. So, you'll need to find the shortest path from any square at elevation a to the square marked E.
+
+Again consider the example from above:
+
+Sabqponm
+abcryxxl
+accszExk
+acctuvwj
+abdefghi
+Now, there are six choices for starting position (five marked a, plus the square marked S that counts as being at elevation a). If you start at the bottom-left square, you can reach the goal most quickly:
+
+...v<<<<
+...vv<<^
+...v>E^^
+.>v>>>^^
+>^>>>>>^
+This path reaches the goal in only 29 steps, the fewest possible.
+
+What is the fewest steps required to move starting from any square with elevation a to the location that should get the best signal?
+
 */
 
 static INPUT_FILE: &str = "data/day12/input.txt";
@@ -15,18 +79,22 @@ pub fn run() {
 
 struct Day {
     part1: bool,
-    left_strings: Vec<Vec<u8>>,
-    right_strings: Vec<Vec<u8>>,
-    strings: Vec<Vec<u8>>,
+    grid: Vec<usize>,
+    dim_x: usize,
+    dim_y: usize,
+    start_index: usize,
+    end_index: usize,
 }
 
 impl Day {
     fn instance(part1: bool) -> Day {
         Day {
             part1: part1,
-            left_strings: Vec::new(),
-            right_strings: Vec::new(),
-            strings: Vec::new(),
+            grid: Vec::new(),
+            dim_x: 0,
+            dim_y: 0,
+            start_index: 0,
+            end_index: 0,
         }
     }
 
@@ -35,16 +103,16 @@ impl Day {
         self.parse(&lines);
 
         if self.part1 {
-            let result1 = self.sum_priorities();
+            let result1 = self.part1();
             println!("Day12: Result1 {result1}");
-            let expected = 7737;
+            let expected = 350;
             if result1 != expected {
                 panic!("Part1 is broken {result1} != {expected}");
             }
         } else {
-            let result2 = self.sum_priorities_three();
+            let result2 = self.part2();
             println!("Day12: Result2 {result2}");
-            let expected = 2697;
+            let expected = 349;
             if result2 != expected {
                 panic!("Part2 is broken {result2} != {expected}");
             }
@@ -52,70 +120,124 @@ impl Day {
     }
 
     fn parse(&mut self, lines: &Vec<String>) {
+        self.dim_y = lines.len();
+        self.dim_x = lines[0].len();
+        self.grid.resize(self.dim_x * self.dim_y, std::usize::MAX);
+        let mut index = 0;
         for line in lines {
-            let rucksack_size = line.len() / 2;
-            let left = line[0..rucksack_size].as_bytes().to_vec();
-            let right = line[rucksack_size..].as_bytes().to_vec();
-            self.left_strings.push(left);
-            self.right_strings.push(right);
-            self.strings.push(line.as_bytes().to_vec());
+            assert_eq!(line.len(), self.dim_x);
+            let bytes = line.as_bytes();
+            for x in 0..self.dim_x {
+                let b = bytes[x];
+                // S = 'a'
+                // E = 'z'
+                let height: usize;
+                if b == ('S' as u8) {
+                    self.start_index = index;
+                    height = 0;
+                } else if b == ('E' as u8) {
+                    self.end_index = index;
+                    height = 25;
+                } else {
+                    height = b as usize - ('a' as usize);
+                }
+                assert!(height <= 25);
+                self.grid[index] = height;
+                index += 1;
+            }
         }
     }
 
-    fn sum_priorities(&self) -> i64 {
-        let count = self.left_strings.len();
-        let mut total = 0_i64;
-        for i in 0..count {
-            let l = &self.left_strings[i];
-            let r = &self.right_strings[i];
-            for b in l {
-                if r.contains(b) {
-                    let c = *b;
-                    if c >= 'a' as u8 && c <= 'z' as u8 {
-                        let priority = c - 'a' as u8;
-                        total += priority as i64;
-                        total += 1;
-                    } else {
-                        let priority = c - 'A' as u8;
-                        total += priority as i64;
-                        total += 27;
-                    }
-                    break;
+    fn find_shortest_path(&self, starting_point: usize, max_length: usize) -> usize {
+        // nodes.push (index:start_index, len:0)
+
+        // while !nodes.empty
+        //   node = nodes.pop;
+        //   if node == end_index : min_len = min(len,min_len) : continue
+        //   if up is valid : push up, len +=1
+        //   if down is valid : push down, len +=1
+        //   if left is valid : push left, len +=1
+        //   if right is valid : push right, len +=1
+        let mut nodes: Vec<(usize, usize)> = Vec::new();
+        let mut visited_grid: Vec<usize> = Vec::new();
+        let dim_x = self.dim_x;
+        let dim_y = self.dim_y;
+        visited_grid.resize(dim_x * dim_y, std::usize::MAX);
+        let mut min_length = std::usize::MAX;
+        nodes.push((starting_point, 0));
+        while !nodes.is_empty() {
+            let node = nodes.pop().unwrap();
+            let current_index = node.0;
+            let current_length = node.1;
+            // if node == end_index : min_len = min(len,min_len) : continue
+            if current_index == self.end_index {
+                if current_length < min_length {
+                    min_length = current_length;
+                }
+                continue;
+            }
+            if current_length >= visited_grid[current_index] {
+                continue;
+            }
+            let new_length = current_length + 1;
+            if new_length >= max_length {
+                continue;
+            }
+            visited_grid[current_index] = current_length;
+            let current_height = self.grid[current_index];
+            let max_height = current_height + 1;
+            let x = current_index % dim_x;
+            let y = current_index / dim_x;
+            assert_eq!(y * dim_x + x, current_index);
+            // if up is valid : push up, len +=1
+            if current_index >= dim_x {
+                let new_index = current_index - dim_x;
+                if self.grid[new_index] <= max_height {
+                    nodes.push((new_index, new_length));
+                }
+            }
+            // if down is valid : push down, len +=1
+            if y < dim_y - 1 {
+                let new_index = current_index + dim_x;
+                if self.grid[new_index] <= max_height {
+                    nodes.push((new_index, new_length));
+                }
+            }
+            // if left is valid : push left, len +=1
+            if x > 0 {
+                let new_index = current_index - 1;
+                if self.grid[new_index] <= max_height {
+                    nodes.push((new_index, new_length));
+                }
+            }
+            // if right is valid : push right, len +=1
+            if x < dim_x - 1 {
+                let new_index = current_index + 1;
+                if self.grid[new_index] <= max_height {
+                    nodes.push((new_index, new_length));
                 }
             }
         }
-        return total;
+        return min_length;
     }
 
-    fn sum_priorities_three(&self) -> i64 {
-        let count = self.left_strings.len();
-        let mut total = 0_i64;
-        for i in (0..count).step_by(3) {
-            let base = &self.strings[i];
-            for b in base {
-                let mut found = 0;
-                for j in 1..3 {
-                    let other = &self.strings[i + j];
-                    if other.contains(b) {
-                        found += 1;
-                    }
-                }
-                if found == 2 {
-                    let c = *b;
-                    if c >= 'a' as u8 && c <= 'z' as u8 {
-                        let priority = c - 'a' as u8;
-                        total += priority as i64;
-                        total += 1;
-                    } else {
-                        let priority = c - 'A' as u8;
-                        total += priority as i64;
-                        total += 27;
-                    }
-                    break;
+    fn part1(&self) -> usize {
+        let length = self.find_shortest_path(self.start_index, std::usize::MAX);
+        return length;
+    }
+
+    fn part2(&self) -> usize {
+        let mut min_length = std::usize::MAX;
+        for i in 0..self.grid.len() {
+            let h = self.grid[i];
+            if h == 0 {
+                let length = self.find_shortest_path(i, min_length);
+                if length < min_length {
+                    min_length = length;
                 }
             }
         }
-        return total;
+        return min_length;
     }
 }
 
@@ -126,33 +248,41 @@ mod tests {
 
     #[test]
     fn part1() {
-        let input: Vec<&str> = vec![
-            "vJrwpWtwJgWrhcsFMMfFFhFp",
-            "jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL",
-            "PmmdzqPrVvPwwTWBwg",
-            "wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn",
-            "ttgJtRGJQctTZtZT",
-            "CrZsJsPPZsGzwwsLwLmpwMDw",
-        ];
-        let lines = str_array_to_string_array(input);
+        let input: (Vec<&str>, usize) = (
+            vec!["Sabqponm", "abcryxxl", "accszExk", "acctuvwj", "abdefghi"],
+            31,
+        );
+        let lines = str_array_to_string_array(input.0);
         let mut day = Day::instance(true);
         day.parse(&lines);
-        assert_eq!(day.sum_priorities(), 157);
+        assert_eq!(day.part1(), input.1);
     }
 
     #[test]
     fn part2() {
-        let input: Vec<&str> = vec![
-            "vJrwpWtwJgWrhcsFMMfFFhFp",
-            "jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL",
-            "PmmdzqPrVvPwwTWBwg",
-            "wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn",
-            "ttgJtRGJQctTZtZT",
-            "CrZsJsPPZsGzwwsLwLmpwMDw",
-        ];
-        let lines = str_array_to_string_array(input);
+        let input: (Vec<&str>, usize) = (
+            vec!["Sabqponm", "abcryxxl", "accszExk", "acctuvwj", "abdefghi"],
+            29,
+        );
+        let lines = str_array_to_string_array(input.0);
         let mut day = Day::instance(true);
         day.parse(&lines);
-        assert_eq!(day.sum_priorities_three(), 70);
+        assert_eq!(day.part2(), input.1);
     }
 }
+
+/*
+
+Walk graph
+
+nodes.push (index:start_index, len:0)
+
+while !nodes.empty
+  node = nodes.pop;
+  if node == end_index : min_len = min(len,min_len) : continue
+  if up is valid : push up, len +=1
+  if down is valid : push down, len +=1
+  if left is valid : push left, len +=1
+  if right is valid : push right, len +=1
+
+*/
